c4bf18876100cd8926d6bd44415f1c83af507f2a
nmskm240 <UTC.AM240@gmail.com>
2023-07-29 14:52:05 +0900

    implements view

--- lib/presentation/pages/wallet/wallet_page.dart
+++ lib/presentation/pages/wallet/wallet_page.dart
@@ -4,15 +4,23 @@ import 'package:flutter/material.dart';
 // Package imports:
 import 'package:auto_route/auto_route.dart';
 
+// Project imports:
+import 'package:local_pay/presentation/pages/wallet/widget/wallet_balance_card.dart';
+
 @RoutePage()
 class WalletPage extends StatelessWidget {
   const WalletPage({super.key});
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Wallet"),
+      ),
+      body: WalletBalanceCard(
+        amount: 0,
+        onPressedDetail: () {},
+        onPressedAdd: () {},
       ),
     );
   }
